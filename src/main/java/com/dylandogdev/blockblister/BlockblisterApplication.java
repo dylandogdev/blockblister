9c188561c284cd49a8709006b75c7f43fdf720dd
Dylan Glenn <dglenn@myseiubenefits.org>
2020-11-20 14:53:06 -0800

    initial save

---
+++ src/main/java/com/dylandogdev/blockblister/BlockblisterApplication.java
@@ -0,0 +1,22 @@
+package com.dylandogdev.blockblister;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BlockblisterApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockblisterApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
---
+++ src/test/java/com/dylandogdev/blockblister/BlockblisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylandogdev.blockblister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockblisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
